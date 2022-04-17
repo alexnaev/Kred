70abc2783e45fad4f33124a772b59d9c23cb6f87
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-04-17 16:31:40 +0200

    Added code page

--- Kred/Models/LoanPayment.cs
+++ Kred/Models/LoanPayment.cs
@@ -5,11 +5,7 @@ namespace Kred.Models
     public class LoanPayment
     {
         public int Month { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
         public decimal Payment { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:C0}")]
         public decimal MonthlyPrincipal { get; set; }
         public decimal MonthlyInterest { get; set; }
         public decimal TotalInterest { get; set; }
